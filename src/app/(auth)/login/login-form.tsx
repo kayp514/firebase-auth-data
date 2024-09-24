de4e9e254a9a4a58bd499c5d21813a11936c5810
kayp514 <pa.ka12@outlook.com>
2024-09-23 22:11:55 -0400

    change #3 create account button

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -3,13 +3,14 @@
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { signIn } from '@/app/lib/auth'
+import { useTheme } from 'next-themes'
 
 export default function LoginForm() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
   const router = useRouter()
-
+  const { theme } = useTheme()
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
     setError('')
@@ -38,7 +39,7 @@ export default function LoginForm() {
               required
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className={`block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset   focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6 ${theme === 'dark' ? 'bg-white/5 text-white ring-white/10 focus:ring-indigo-500' : 'text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-indigo-600'}`}
             />
           </div>
         </div>
@@ -63,7 +64,7 @@ export default function LoginForm() {
               required
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className={`block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset   focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6 ${theme === 'dark' ? 'bg-white/5 ring-white/10 focus:ring-indigo-500' : 'text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-indigo-600'}`}
             />
           </div>
         </div>
@@ -79,7 +80,12 @@ export default function LoginForm() {
           </button>
         </div>
       </form>
-
+      <p className={`mt-10 text-center text-sm  ${theme === 'dark' ? 'text-gray-400' : 'text-gray-500'}`}>
+            don't have an account?{' '}
+            <a href="#" className="font-semibold leading-6 text-indigo-400 hover:text-indigo-300">
+              Create an account
+            </a>
+          </p>
     </div>
   )
 }
