5f939eaa847fce37bd332af7fedad80931dcdad6
kayp514 <pa.ka12@outlook.com>
2024-11-25 18:06:04 -0500

    change 58

--- src/app/lib/firebaseClient.ts
+++ src/app/lib/firebaseClient.ts
@@ -4,15 +4,10 @@ import { initializeApp } from 'firebase/app'
 import {  getAuth, setPersistence, browserSessionPersistence, inMemoryPersistence, browserLocalPersistence, connectAuthEmulator } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
-const clientConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-}
+//const clientConfig = {}
+
+const response = await fetch('https://api.ternsecure.com/api/fireconfig')
+const clientConfig = await response.json()
 
 
 const clientApp = initializeApp(clientConfig)
