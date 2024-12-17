464c1024aeb8e9f16f91da3b65d70487075f8b8e
kayp514 <pa.ka12@outlook.com>
2024-12-17 15:21:25 -0500

    change 70

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
     compiler: {
       removeConsole: process.env.NODE_ENV === 'production',
     },
+
+    async rewrites() {
+      return [
+        {
+          source: '/__/auth/:path*',
+          destination: 'https://dev-coffeeconnect-v1.firebaseapp.com/__/auth/:path*',
+        },
+      ]
+    },
   };
   
   export default nextConfig;
