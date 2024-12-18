c94fa8f1eace14273b194acf4c6ae6809da9b6af
kayp514 <pa.ka12@outlook.com>
2024-12-17 19:48:58 -0500

    change 73.2

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -30,8 +30,11 @@ export async function signInWithRedirectGoogle() {
   const provider = new GoogleAuthProvider()
   provider.setCustomParameters({
     login_hint: 'user@example.com',
-    prompt: 'select_account'
-  })
+    prompt: 'select_account',
+    redirect_uri: 'https://ternsecure.com/auth/callback', 
+    redirectUrl: 'https://ternsecure.com/auth/callback',
+    context_uri: 'https://ternsecure.com/auth/callback',
+  })  
 
   try {
     await signInWithRedirect(clientAuth, provider)
